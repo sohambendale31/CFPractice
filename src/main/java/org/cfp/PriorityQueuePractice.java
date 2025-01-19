6ab7e14dbcc14649f976a93029306cc7e25829c5
soham <sohambendale31@gmail.com>
2025-01-19 19:02:53 +0530

    PriorityQueuePractice basics

--- src/main/java/org/cfp/PriorityQueuePractice.java
+++ src/main/java/org/cfp/PriorityQueuePractice.java
@@ -1,5 +1,18 @@
 package org.cfp;
 
+import java.util.PriorityQueue;
+
 public class PriorityQueuePractice {
-    public void execute(){}
+    public void execute(){
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        minPQ.add(9);
+        minPQ.add(1);
+        minPQ.add(7);
+        minPQ.add(3);
+        minPQ.forEach((Integer val) -> System.out.println(val));
+        while (!minPQ.isEmpty()){
+            int removedval = minPQ.poll();
+            System.out.println("removed from top : " + removedval);
+        }
+    }
 }
