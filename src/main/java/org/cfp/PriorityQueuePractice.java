c2499637d1e3f3ac1acab51b565387dec5f4e801
soham <sohambendale31@gmail.com>
2025-01-19 19:10:23 +0530

    PriorityQueuePractice basics max heap

--- src/main/java/org/cfp/PriorityQueuePractice.java
+++ src/main/java/org/cfp/PriorityQueuePractice.java
@@ -14,5 +14,18 @@ public class PriorityQueuePractice {
             int removedval = minPQ.poll();
             System.out.println("removed from top : " + removedval);
         }
+
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>((Integer a, Integer b) -> b-a);
+        maxPQ.add(2);
+        maxPQ.add(9);
+        maxPQ.add(5);
+        maxPQ.add(1);
+        maxPQ.add(8);
+        maxPQ.add(3);
+        maxPQ.forEach((Integer val) -> System.out.println(val));
+        while (!maxPQ.isEmpty()){
+            int removedvalmax = maxPQ.poll();
+            System.out.println("removed from top : " + removedvalmax);
+        }
     }
 }
